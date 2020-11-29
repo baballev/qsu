Fixed numerical unstability issues, now launching a new training with more stables hyperparameters

main.py
@@ -14,10 +14,11 @@ from trainer import Trainer
 torch.cuda.empty_cache()
 
 BATCH_SIZE = 10
-LEARNING_RATE = 0.001
+LEARNING_RATE = 0.0001
 GAMMA = 0.999
-TAU = 0.001
+TAU = 0.0001
 MAX_STEPS = 50000
+WIDTH = 735
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -32,34 +33,34 @@ def threaded_mouse_move(x, y, t, human_clicker):
     return
 
 
-threshold = torch.tensor(512.0, device=device)
+threshold = torch.tensor(0.5, device=device)
 
 
 def perform_action(action, human_clicker):
-    x, y = action[0][0], (600.0/1024.0) * action[0][1]
+    x, y = action[0][0],  action[0][1]
     if action[0][2] > threshold:
         pyautogui.mouseDown(button='left')
-        left = 1024.0
+        left = 1.0
     else:
         pyautogui.mouseUp(button='left')
         left = 0.0
     if action[0][3] > threshold:  # ToDo: variabiliser
         pyautogui.mouseDown(button='right')
-        right = 1024.0
+        right = 1.0
     else:
         pyautogui.mouseUp(button='right')
         right = 0.0
-    if y < 75:
-        thread = Thread(target=threaded_mouse_move, args=(min(int(x), 840), 75+26, 0.08, human_clicker))
+    if y*WIDTH < 54:
+        thread = Thread(target=threaded_mouse_move, args=(min(int(x*WIDTH)+145, 850), 54+26, 0.1, human_clicker))
         thread.start()
-    elif y > 552:
-        thread = Thread(target=threaded_mouse_move, args=(min(int(x), 830), 552+26, 0.08, human_clicker))
+    elif y*WIDTH > 560:
+        thread = Thread(target=threaded_mouse_move, args=(min(int(x*WIDTH)+145, 830), 560+26, 0.1, human_clicker))
         thread.start()
-    elif x > 840:
-        thread = Thread(target=threaded_mouse_move, args=(840, int(y)+26, 0.08, human_clicker))
+    elif x*WIDTH > 705:
+        thread = Thread(target=threaded_mouse_move, args=(705+145, int(y*WIDTH)+26, 0.1, human_clicker))
         thread.start()
     else:
-        thread = Thread(target=threaded_mouse_move, args=(max(int(x), 100), int(y)+26, 0.08, human_clicker))
+        thread = Thread(target=threaded_mouse_move, args=(max(int(x*WIDTH)+145, 145), int(y*WIDTH)+26, 0.1, human_clicker))
         thread.start()
     return torch.tensor([[x, y, left, right]], device=device)
 
@@ -75,21 +76,21 @@ def train(episode_nb, learning_rate, load_weights=None, save_name='tests'):
     utils.osu_routines.move_to_songs(star=1)
     utils.osu_routines.enable_nofail()
 
-    trainer = Trainer()  # ToDo: Modify parameters
+    trainer = Trainer(load_weights=load_weights)  # ToDo: Modify parameters
     k = 0
     for i in range(episode_nb):
         utils.osu_routines.launch_random_beatmap()
 
         state = (utils.screen.get_game_screen(trainer.screen).unsqueeze_(0).sum(1, keepdim=True)/3.0)
         previous_score = 0
-        controls_state = torch.tensor([[500.0, 320.0, 0.0, 0.0]], device=device)
+        controls_state = torch.tensor([[0.5, 0.5, 0.0, 0.0]], device=device)
         episode_average_reward = 0.0
         start = time.time()
         for step in range(MAX_STEPS):
             k += 1
             action = trainer.select_exploration_action(state, controls_state)
             new_controls_state = perform_action(action, trainer.hc)
-            time.sleep(0.02)
+            # time.sleep(0.02)
             new_state = (utils.screen.get_game_screen(trainer.screen).unsqueeze_(0).sum(1, keepdim=True)/3.0)
             score = utils.OCR.get_score(trainer.screen, trainer.ocr, wndw)
             if (step < 15 and score == -1) or (score - previous_score > 100000):
@@ -146,7 +147,7 @@ def train(episode_nb, learning_rate, load_weights=None, save_name='tests'):
 
 
 if __name__ == '__main__':
-    weights_path = ('./weights/actortraining28-11-2020-50.pt', './weights/critictraining28-11-2020-50.pt')
-    save_name = 'training29-11-2020-'
-    train(500, LEARNING_RATE, save_name=save_name, load_weights=None)
+    weights_path = ('./weights/actortraining29-11-2020-200.pt', './weights/critictraining29-11-2020-200.pt')
+    save_name = 'training_29-11-2020-'
+    train(200, LEARNING_RATE, save_name=save_name, load_weights=None)
 

models.py
@@ -16,22 +16,22 @@ class Actor(nn.Module):
         self.channels = channels
         self.action_dim = action_dim
 
-        self.conv1 = nn.Conv2d(self.channels, 8, kernel_size=5, stride=2)
+        self.conv1 = nn.Conv2d(self.channels, 8, kernel_size=9, stride=2)
         self.bn1 = nn.BatchNorm2d(8)
-        self.conv2 = nn.Conv2d(8, 16, kernel_size=5, stride=2)
+        self.conv2 = nn.Conv2d(8, 16, kernel_size=7, stride=2)
         self.bn2 = nn.BatchNorm2d(16)
-        self.conv3 = nn.Conv2d(16, 16, kernel_size=5, stride=2)
-        self.bn3 = nn.BatchNorm2d(16)
-        self.conv4 = nn.Conv2d(16, 8, kernel_size=3, stride=2)
-        self.bn4 = nn.BatchNorm2d(8)
+        self.conv3 = nn.Conv2d(16, 24, kernel_size=5, stride=2)
+        self.bn3 = nn.BatchNorm2d(24)
+        self.conv4 = nn.Conv2d(24, 12, kernel_size=3, stride=2)
+        self.bn4 = nn.BatchNorm2d(12)
 
         def conv2d_size_out(size, kernel_size=5, stride=2):
             return (size - (kernel_size - 1) - 1) // stride + 1
 
-        convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(conv2d_size_out(width))), kernel_size=3)
-        convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(conv2d_size_out(height))), kernel_size=3)
+        convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(conv2d_size_out(width, kernel_size=9), kernel_size=7)), kernel_size=3)
+        convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(conv2d_size_out(height, kernel_size=9), kernel_size=7)), kernel_size=3)
 
-        self.fc1 = nn.Linear(convh * convw * 8, 2048)
+        self.fc1 = nn.Linear(convh * convw * 12, 2048)
         self.fc2 = nn.Linear(2048, 512)
         self.fcc1 = nn.Linear(control_dim, 128)
         self.fcc2 = nn.Linear(128, 256)
@@ -48,10 +48,10 @@ class Actor(nn.Module):
         x = F.leaky_relu(self.fc2(x))
         y = F.leaky_relu(self.fcc1(controls_state))
         y = F.leaky_relu(self.fcc2(y))
-        x = torch.cat((x, y), dim=1)
-        x = F.leaky_relu(self.fc3(x))
-        x = torch.sigmoid(self.fc4(x))
-        return x * self.width
+        z = torch.cat((x, y), dim=1)
+        z = F.leaky_relu(self.fc3(z))
+        z = self.fc4(z)
+        return z
 
 
 class Critic(nn.Module):
@@ -63,22 +63,22 @@ class Critic(nn.Module):
         self.channels = channels
         self.action_dim = action_dim
 
-        self.convs1 = nn.Conv2d(self.channels, 8, kernel_size=5, stride=2)
+        self.convs1 = nn.Conv2d(self.channels, 8, kernel_size=9, stride=2)
         self.bns1 = nn.BatchNorm2d(8)
-        self.convs2 = nn.Conv2d(8, 16, kernel_size=5, stride=2)
+        self.convs2 = nn.Conv2d(8, 16, kernel_size=7, stride=2)
         self.bns2 = nn.BatchNorm2d(16)
-        self.convs3 = nn.Conv2d(16, 16, kernel_size=5, stride=2)
-        self.bns3 = nn.BatchNorm2d(16)
-        self.convs4 = nn.Conv2d(16, 8, kernel_size=3, stride=2)
-        self.bns4 = nn.BatchNorm2d(8)
+        self.convs3 = nn.Conv2d(16, 24, kernel_size=5, stride=2)
+        self.bns3 = nn.BatchNorm2d(24)
+        self.convs4 = nn.Conv2d(24, 12, kernel_size=3, stride=2)
+        self.bns4 = nn.BatchNorm2d(12)
 
         def conv2d_size_out(size, kernel_size=5, stride=2):
             return (size - (kernel_size - 1) - 1) // stride + 1
 
-        convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(conv2d_size_out(width))), kernel_size=3)
-        convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(conv2d_size_out(height))), kernel_size=3)
+        convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(conv2d_size_out(width, kernel_size=9), kernel_size=7)), kernel_size=3)
+        convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(conv2d_size_out(height, kernel_size=9), kernel_size=7)), kernel_size=3)
 
-        self.fcs4 = nn.Linear(convh * convw * 8, 2048)
+        self.fcs4 = nn.Linear(convh * convw * 12, 2048)
         self.fcs5 = nn.Linear(2048, 512)
 
         self.fca1 = nn.Linear(self.action_dim, 128)

trainer.py
@@ -27,7 +27,7 @@ def soft_update(target, source, tau):  # y = TAU * x  +  (1 - TAU) * y
 
 class Trainer:
 
-    def __init__(self, batch_size=5, lr=0.001, tau=0.001, gamma=0.999, load_weights=None, width=735, height=546): # ToDo: change width and height of state
+    def __init__(self, batch_size=5, lr=0.0001, tau=0.0001, gamma=0.999, load_weights=None, width=735, height=546): # ToDo: change width and height of state
         self.batch_size = batch_size
         self.lr = lr
         self.tau = tau
@@ -45,12 +45,12 @@ class Trainer:
             hard_copy(self.target_actor, self.actor)
             hard_copy(self.target_critic, self.critic)
 
-        self.noise = utils.noise.OrnsteinUhlenbeckActionNoise(mu=torch.tensor([0.0, 0.0, 0.0, 0.0], device=device), sigma=150.0, theta=15, x0=torch.tensor([0.0, 0.0, 0.0, 0.0], device=device))
+        self.noise = utils.noise.OrnsteinUhlenbeckActionNoise(mu=torch.tensor([0.0, 0.0, 0.0, 0.0], device=device), sigma=0.2, theta=0.15, x0=torch.tensor([0.0, 0.0, 0.0, 0.0], device=device))
 
         self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), self.lr)
         self.critic_optimizer = torch.optim.Adam(self.critic.parameters(), self.lr)
 
-        self.memory = ReplayMemory(1500)  # The larger the better because then the transitions have more chances to be uncorrelated
+        self.memory = ReplayMemory(2000)  # The larger the better because then the transitions have more chances to be uncorrelated
 
         self.screen = utils.screen.init_screen(capture_output="pytorch_float_gpu")
         self.ocr = utils.OCR.init_OCR()
@@ -59,7 +59,11 @@ class Trainer:
     def select_exploration_action(self, state, controls_state):  # Check if the values are ok
         action = self.actor(state, controls_state).detach()
         new_action = action + self.noise.get_noise()  # ToDo: Check the noise progression with print or plot
-        return new_action  # ToDO: Code exploitation policy action
+        return new_action
+
+    def select_exploitation_action(self, state, controls_state):
+        action = self.target_actor(state, controls_state).detach()
+        return action
 
     def save_model(self, file_name, num=0):
         torch.save(self.target_actor.state_dict(), './weights/actor' + file_name + str(num) + '.pt')
@@ -70,7 +74,9 @@ class Trainer:
 
     def load_models(self, weights_path):
         self.actor.load_state_dict(torch.load(weights_path[0]))
+        print('Loaded actor weights from: ' + weights_path[0])
         self.critic.load_state_dict(torch.load(weights_path[1]))
+        print('Loaded critic weights from :' + weights_path[1])
         hard_copy(self.target_actor, self.actor)
         hard_copy(self.target_critic, self.critic)
         return
@@ -104,3 +110,8 @@ class Trainer:
        # Todo: Add verbose ?
 
 
+if __name__ == '__main__':
+    trainer = Trainer()
+    while True:
+        time.sleep(0.5)
+        print(trainer.noise.get_noise())

utils/OCR.py
@@ -137,17 +137,21 @@ counter = 0
 
 def get_score(screen, ocr, wndw):
     global counter
-    score_img = utils.screen.get_screen_region(screen, region=SCORE_REGION)
-    if counter % 5 == 0:
-        if not(score_img.sum()) or win32gui.GetWindowText(wndw) == 'osu!':
+    with torch.no_grad():
+        score_img = utils.screen.get_screen_region(screen, region=SCORE_REGION)
+        if counter % 5 == 0:
+            if not(score_img.sum()) or win32gui.GetWindowText(wndw) == 'osu!':
+                return -1
+
+        score_img = torch.stack([score_img[:, :, j*18:(j+1)*18] for j in range(8)], 0)
+        if score_img.shape[2] > 27:
             return -1
-    score_img = torch.stack([score_img[:, :, j*18:(j+1)*18] for j in range(8)], 0)
-    score_img = ocr(score_img)
-    _, indices = torch.max(score_img, 1)
-    s = 0.0
-    for n, indic in enumerate(indices.float()):
-        s += indic * 10**(7-n)
-    counter += 1
+        score_img = ocr(score_img)
+        _, indices = torch.max(score_img, 1)
+        s = 0.0
+        for n, indic in enumerate(indices.float()):
+            s += indic * 10**(7-n)
+        counter += 1
     return s
 
 

utils/noise.py
@@ -4,6 +4,7 @@ import torch
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+
 class AdaptiveParamNoiseSpec(object):
     def __init__(self, initial_stddev=0.1, desired_action_stddev=0.1, adoption_coefficient=1.01):
         self.initial_stddev = initial_stddev
@@ -65,7 +66,7 @@ class OrnsteinUhlenbeckActionNoise(ActionNoise):
         return x
 
     def reset(self):
-        self.x_prev = self.x0 if self.x0 is not None else torch.zeros_like(self.mu).to(device)
+        self.x_prev = self.x0 if self.x0 is not None else torch.zeros_like(self.mu, device=device)
 
     def __repr__(self):
         return 'OrnsteinUhlenbeckActionNoise(mu={}, sigma={})'.format(self.mu, self.sigma)