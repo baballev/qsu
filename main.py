Fixed some divergences issues due to giving the pixel coordinates without normalizing it. engaging in a long training process

main.py
@@ -17,7 +17,7 @@ torch.cuda.empty_cache()
 BATCH_SIZE = 5
 LEARNING_RATE = 0.00001
 GAMMA = 0.999
-TAU = 0.00001
+TAU = 0.0001
 MAX_STEPS = 50000
 WIDTH = 735
 HEIGHT = 546
@@ -80,7 +80,7 @@ def get_reward(score, previous_score, acc, previous_acc, step):
 
 
 ## Training
-def train(episode_nb, learning_rate, batch_size=BATCH_SIZE, load_weights=None, save_name='tests', beatmap_name=None, star=1):
+def train(episode_nb, learning_rate, batch_size=BATCH_SIZE, load_weights=None, save_name='tests', beatmap_name=None, star=1, frequency=7.5):
     # Osu routine
     process, wndw = utils.osu_routines.start_osu()
     utils.osu_routines.move_to_songs(star=star)
@@ -105,6 +105,7 @@ def train(episode_nb, learning_rate, batch_size=BATCH_SIZE, load_weights=None, s
         thread = None
         #logger = open('./benchmark/log.txt', 'w+')
         for step in range(MAX_STEPS):
+            step_time_prev = time.time()
             k += 1
             with torch.no_grad():
                 action = trainer.select_exploration_action(state, controls_state, i, step)
@@ -116,7 +117,7 @@ def train(episode_nb, learning_rate, batch_size=BATCH_SIZE, load_weights=None, s
                 current_screen = (utils.screen.get_game_screen(trainer.screen).unsqueeze_(0).sum(1, keepdim=True)/3.0)
                 score, acc = utils.OCR.get_score_acc(trainer.screen, trainer.score_ocr, trainer.acc_ocr, wndw)
                 new_x, new_y = pyautogui.position()
-                new_controls_state = torch.cat((torch.tensor([[new_x, new_y]], dtype=torch.float32, device=device), new_controls_state), 1)
+                new_controls_state = torch.cat((torch.tensor([[new_x/WIDTH, new_y/HEIGHT]], dtype=torch.float32, device=device), new_controls_state), 1)
                 if (step < 15 and score == -1) or (score - previous_score > 5*(previous_score+100)):
                     score = previous_score
                 if step < 15 and acc == -1:
@@ -138,6 +139,7 @@ def train(episode_nb, learning_rate, batch_size=BATCH_SIZE, load_weights=None, s
                 #trainer.memory.push(state, action, reward, new_state, controls_state, new_controls_state)
             if thread is not None:
                 thread.join()
+
             thread = Thread(target=trainer.optimize)
             thread.start()
             #trainer.optimize()
@@ -156,6 +158,12 @@ def train(episode_nb, learning_rate, batch_size=BATCH_SIZE, load_weights=None, s
 
             if done:
                 break
+
+            step_time_curr = time.time()
+            if step_time_curr - step_time_prev < 1/frequency:
+                dt = 1/frequency - (step_time_curr - step_time_prev)
+                time.sleep(dt/1.015)
+
 
         end = time.time()
         delta_t = end - start
@@ -196,7 +204,7 @@ def train(episode_nb, learning_rate, batch_size=BATCH_SIZE, load_weights=None, s
 
 
 if __name__ == '__main__':
-    weights_path = ('./weights/actorcheatreal_07-12-2020-9.pt', './weights/criticcheatreal_07-12-2020-9.pt')
-    save_name = '_bis_07-12-2020-'
-    train(8, LEARNING_RATE, save_name=save_name, load_weights=weights_path, beatmap_name="cheatreal", star=3)
+    weights_path = ('./weights/actorbongo_08-12-2020-144.pt', './weights/criticbongo_08-12-2020-144.pt')
+    save_name = '_bis_08-12-2020-'
+    train(90, LEARNING_RATE, save_name=save_name, load_weights=weights_path, beatmap_name="bongo", star=2)
 

models.py
@@ -33,8 +33,8 @@ class Actor(nn.Module):
 
         self.fc1 = nn.Linear(convh * convw, 512)
         self.fc2 = nn.Linear(512, 256)
-        self.fcc1 = nn.Linear(control_dim, 32)
-        self.fc3 = nn.Linear(256 + 32, 128)
+        self.fcc1 = nn.Linear(control_dim, 12)
+        self.fc3 = nn.Linear(256 + 12, 128)
         self.fc4 = nn.Linear(128, 64)
         self.fc5 = nn.Linear(64, 32)
         self.fc6 = nn.Linear(32, action_dim)
@@ -85,9 +85,9 @@ class Critic(nn.Module):
 
         self.fca1 = nn.Linear(self.action_dim, 32)
 
-        self.fc1 = nn.Linear(control_dim, 32)
+        self.fc1 = nn.Linear(control_dim, 12)
 
-        self.ffc1 = nn.Linear(192, 1)
+        self.ffc1 = nn.Linear(172, 1)
 
     def forward(self, state, controls_state, action):  # Compute an approximate Q(s, a) value function
         x = F.leaky_relu(self.convs1(state))

trainer.py
@@ -47,10 +47,10 @@ class Trainer:
             hard_copy(self.target_actor, self.actor)
             hard_copy(self.target_critic, self.critic)
 
-        self.noise = utils.noise.OrnsteinUhlenbeckActionNoise(mu=torch.tensor([0.0, 0.0, 0.0, 0.0], device=device), sigma=0.2, theta=0.15, x0=torch.tensor([0.0, 0.0, 0.0, 0.0], device=device))
+        self.noise = utils.noise.OrnsteinUhlenbeckActionNoise(mu=torch.tensor([0.0, 0.0, 0.0, 0.0], device=device), sigma=0.25, theta=0.15, x0=torch.tensor([0.0, 0.0, 0.0, 0.0], device=device))
 
-        self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), self.lr/10.0, eps=0.001)
-        self.critic_optimizer = torch.optim.Adam(self.critic.parameters(), self.lr, eps=0.001)
+        self.actor_optimizer = torch.optim.AdamW(self.actor.parameters(), self.lr/2.0, eps=0.000001, weight_decay=0.0001)
+        self.critic_optimizer = torch.optim.AdamW(self.critic.parameters(), self.lr, eps=0.000001, weight_decay=0.0001)
 
         self.memory = ReplayMemory(1500)  # The larger the better because then the transitions have more chances to be uncorrelated
 
@@ -61,7 +61,7 @@ class Trainer:
 
     def select_exploration_action(self, state, controls_state, episode_num=0, step_num=1):  # Check if the values are ok
         action = self.actor(state, controls_state).detach()
-        if step_num % 50 == 0:
+        if step_num % 300 == 0:
             print(action)
         new_action = action + DECAY**(episode_num +1) * self.noise.get_noise()
         return torch.clip(new_action, 0.0, 1.0)
@@ -100,7 +100,7 @@ class Trainer:
         loss_critic = F.smooth_l1_loss(y_predicted, y_expected) #TODO: try mse?
         self.critic_optimizer.zero_grad()
         loss_critic.backward()
-        #torch.nn.utils.clip_grad_norm_(self.critic.parameters(), 1.0)
+        torch.nn.utils.clip_grad_value_(self.critic.parameters(), 0.01)
         self.critic_optimizer.step()
 
         # ---------- Actor ----------
@@ -109,7 +109,7 @@ class Trainer:
         self.actor_optimizer.zero_grad()
         loss_actor.backward()
         #print(self.actor.fc6.weight.grad)
-        torch.nn.utils.clip_grad_norm_(self.actor.parameters(), 1.0)
+        torch.nn.utils.clip_grad_value_(self.actor.parameters(), 0.01)
         self.actor_optimizer.step()
 
         soft_update(self.target_actor, self.actor, self.tau)

utils/noise.py
@@ -51,7 +51,7 @@ class NormalActionNoise(ActionNoise):
 
 # Based on http://math.stackexchange.com/questions/1287634/implementing-ornstein-uhlenbeck-in-matlab
 class OrnsteinUhlenbeckActionNoise(ActionNoise):
-    def __init__(self, mu, sigma, theta=.15, dt=torch.tensor([0.15], device=device), x0=None):
+    def __init__(self, mu, sigma, theta=.15, dt=torch.tensor([0.12], device=device), x0=None):
         self.theta = theta
         self.mu = mu
         self.sigma = sigma