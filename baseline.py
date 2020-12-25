Added busy waiting to regulate number of time steps per second, replaced gradient clipping with norm clipping like in Dueling networks, fixed small issue in osu_routines when starting the game, fixed baseline atari benchmark

baseline.py
@@ -423,9 +423,9 @@ if __name__ == '__main__':
     f = open('statistics.pkl', 'rb')
     stat = pickle.load(f)
     f.close()
-    x = np.linspace(len(stat["mean_episode_rewards"]), 0, len(stat["mean_episode_rewards"]))
+    x = np.linspace(0, len(stat["mean_episode_rewards"]), len(stat["mean_episode_rewards"]))
     import matplotlib.pyplot as plt
     fig = plt.figure()
     ax = plt.subplot(111)
-    ax.plot(x, np.array(stat["mean_episode_rewards"][::-1]))
+    ax.plot(x, np.array(stat["mean_episode_rewards"]))
     plt.show()

environment.py
@@ -103,7 +103,7 @@ class OsuEnv(gym.Env):
         self.previous_acc = torch.tensor(100.0, device=device)
         return torch.tensor([[0.5, 0.5, 0.0, 0.0]], device=device), self.history.unsqueeze(0)
 
-    def observe(self, steps, dt=1/(9.0*3)):
+    def observe(self, steps, dt=1/(9.0*3)):  # Obsoltete
         time.sleep(dt)
         x, y = pyautogui.position()
         left, right = (win32api.GetAsyncKeyState(ord('C')) <= -32767), (win32api.GetAsyncKeyState(ord('V')) <= -32767)
@@ -149,15 +149,15 @@ class OsuEnv(gym.Env):
         time.sleep(0.3)
 
     def change_star(self, star=2):
-        pass # ToDo
+        pass  # ToDo
 
     def stop(self):
         self.reset(0.5)
         self.screen.stop()
         utils.osu_routines.stop_osu(self.process)
 
-    def get_reward(self, score, acc):
-        if acc > self.previous_acc:
+    def get_reward(self, score, acc):  # ToDo: Rethink this reward function with and without nofail
+        if acc > self.previous_acc:    # ToDo: CLip it in [-1, 1] as well as TD-error in the optimization func, not here
             bonus = torch.tensor(0.3, device=device)
         elif acc < self.previous_acc:
             bonus = torch.tensor(-0.3, device=device)

main.py
@@ -16,7 +16,7 @@ pyautogui.MINIMUM_SLEEP = 0.0
 pyautogui.PAUSE = 0.0
 
 BATCH_SIZE = 32
-LEARNING_RATE = 0.0001
+LEARNING_RATE = 0.00005  # Double DQN: 0.00025, Prioritized Replay -> Double DQN / 4
 GAMMA = 0.999
 MAX_STEPS = 25000
 WIDTH = 878
@@ -40,7 +40,7 @@ def print_infos():
     print('YMAX = ' + str(54 + 26 + (Y_DISCRETE - 1) * DISCRETE_FACTOR))
 
 
-def log_episodes(i, q_trainer, steps, delta_t):
+def log_episodes(i, q_trainer, steps, delta_t, k):
     if i <= 1 or i % 30 == 0:
         print(str(steps) + ' steps in ' + str(delta_t) + ' s.' + '  -->   %.4f' % (steps / delta_t) + ' steps / s')
 
@@ -52,20 +52,33 @@ def log_episodes(i, q_trainer, steps, delta_t):
         q_trainer.plotter.fig.savefig('average_loss' + str(i) + '.png')
         q_trainer.avg_reward_plotter.fig.savefig('episode_reward' + str(i) + '.png')
 
+    if i % 30 == 0:
+        print("Episode %d" % (i+1))
+        print("Time step %d" % k)
+        print("Schedule: %f" % q_trainer.scheduler.value(k))
+
+
+def busy_wait(freq, previous_t):
+    while time.time() - previous_t < 1/(freq+0.5):
+        time.sleep(0.001)
+    return time.time()
+
 
 def trainQNetwork(episode_nb, learning_rate, batch_size=BATCH_SIZE, load_weights=None, save_name='tests',
                   beatmap_name=None, star=1, evaluation=False, human_off_policy=False, load_memory=None, no_fail=False,
-                  initial_p=1.0, end_p=0.05, decay_p=2000000, target_update=30000):
-    training_steps = 0
+                  initial_p=1.0, end_p=0.05, decay_p=2000000, target_update=30000, init_k=0, min_experience=25000):
     print_infos()
     if evaluation:
         learning_rate = 0.0
 
-    env = environment.OsuEnv(X_DISCRETE, Y_DISCRETE, DISCRETE_FACTOR, WIDTH, HEIGHT, STACK_SIZE, star=star, beatmap_name=beatmap_name, no_fail=no_fail, skip_pixels=PIXEL_SKIP)
-    q_trainer = QTrainer(env, batch_size=batch_size, lr=learning_rate, gamma=GAMMA, initial_p=initial_p, end_p=end_p, decay_p=decay_p, load_weights=load_weights, load_memory=load_memory)
+    env = environment.OsuEnv(X_DISCRETE, Y_DISCRETE, DISCRETE_FACTOR, WIDTH, HEIGHT, STACK_SIZE, star=star,
+                             beatmap_name=beatmap_name, no_fail=no_fail, skip_pixels=PIXEL_SKIP)
+    q_trainer = QTrainer(env, batch_size=batch_size, lr=learning_rate, gamma=GAMMA, initial_p=initial_p, end_p=end_p,
+                         decay_p=decay_p, load_weights=load_weights, load_memory=load_memory,
+                         min_experience=min_experience, gradient_clipping_norm=10.0)
 
     episode_average_reward = 0.0
-    k = 0
+    k = init_k
     reward = torch.tensor(0.0, device=device)
     for i in range(episode_nb):
         controls_state, state = env.reset(reward)
@@ -75,19 +88,21 @@ def trainQNetwork(episode_nb, learning_rate, batch_size=BATCH_SIZE, load_weights
         thread = None
         start = time.time()
         for steps in range(MAX_STEPS):
+            previous_t = time.time()
             k += 1
             with torch.no_grad():
                 if not human_off_policy:
                     if evaluation:  # Choose greedy policy if tests
                         action = q_trainer.select_action(state, controls_state)
                     else:  # Else choose an epsilon greedy policy with decaying epsilon
-                        sample = random()
-                        if sample > q_trainer.scheduler.value(k):
-                            action = q_trainer.select_action(state, controls_state)
+                        if k > min_experience:
+                            sample = random()
+                            if sample > q_trainer.scheduler.value(k):
+                                action = q_trainer.select_action(state, controls_state)
+                            else:
+                                action = q_trainer.random_action(X_DISCRETE, Y_DISCRETE)
                         else:
-                            x = q_trainer.noise()  # Normal distribution mean=0.5, clipped in [0, 1[ & uniform distrib
-                            action = torch.tensor([int(x[0] * X_DISCRETE) + X_DISCRETE * int(
-                                x[1] * Y_DISCRETE) + X_DISCRETE * Y_DISCRETE * int(x[2] * 4)], device=device)
+                            action = q_trainer.random_action(X_DISCRETE, Y_DISCRETE)
 
                     new_state, new_controls_state, reward, done = env.step(action, steps)
                 else:
@@ -109,6 +124,7 @@ def trainQNetwork(episode_nb, learning_rate, batch_size=BATCH_SIZE, load_weights
             controls_state = new_controls_state
             episode_reward += reward
 
+            previous_t = busy_wait(freq=12.0, previous_t=previous_t)
             if done:
                 break
         end = time.time()
@@ -118,11 +134,10 @@ def trainQNetwork(episode_nb, learning_rate, batch_size=BATCH_SIZE, load_weights
 
         episode_average_reward += episode_reward
         q_trainer.avg_reward_plotter.step(episode_reward)
-        log_episodes(i, q_trainer, steps, delta_t)
+        log_episodes(i, q_trainer, steps, delta_t, k)
 
         if k % target_update == 0:
             q_trainer.target_q_network.load_state_dict(q_trainer.q_network.state_dict())
-            print(k)
 
         if i % 30 == 0 and i > 0:
             print('Mean reward over last 30 episodes: ')
@@ -143,7 +158,7 @@ def trainQNetwork(episode_nb, learning_rate, batch_size=BATCH_SIZE, load_weights
         q_trainer.save_model(tmp, num=episode_nb - 1)
         q_trainer.plotter.fig.savefig('average_loss' + str(episode_nb-1) + '.png')
         q_trainer.avg_reward_plotter.fig.savefig('average_reward' + str(episode_nb-1) + '.png')
-
+    print('k: %d' % k)
     env.stop()
 
 
@@ -153,4 +168,4 @@ if __name__ == '__main__':
     save_name = '_25-12-2020-'
     trainQNetwork(50, LEARNING_RATE, evaluation=False, load_weights=None, beatmap_name="sink", star=7,
                   save_name=save_name, batch_size=BATCH_SIZE, human_off_policy=False, no_fail=True,
-                  initial_p=1.0, end_p=0.05, decay_p=2000000, target_update=30000)
+                  initial_p=1.0, end_p=0.05, decay_p=4000000, target_update=30000, init_k=0, min_experience=50)

trainer.py
@@ -9,7 +9,7 @@ import utils.noise
 import utils.screen
 import utils.OCR
 import utils.info_plot
-import uitls.schedule
+import utils.schedule
 from memory import ReplayMemory
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -127,7 +127,8 @@ class Trainer:
 
 
 class QTrainer:
-    def __init__(self, env, batch_size=32, lr=0.0001, gamma=0.999, initial_p=1.0, end_p=0.05, decay_p=2000000, load_weights=None, load_memory=None):
+    def __init__(self, env, batch_size=32, lr=0.0001, gamma=0.999, initial_p=1.0, end_p=0.05, decay_p=2000000,
+                 load_weights=None, load_memory=None, min_experience=25000, gradient_clipping_norm=10.0):
         self.batch_size = batch_size
         self.lr = lr
         self.gamma = gamma
@@ -142,11 +143,11 @@ class QTrainer:
         self.optimizer = torch.optim.RMSprop(self.q_network.parameters(), self.lr, eps=0.01, alpha=0.95)
         self.scheduler = utils.schedule.LinearSchedule(decay_p, end_p, initial_p)
 
-
         if load_memory is None:
             self.memory = ReplayMemory(1000000) # TODO: Optimize memory
         else:
             self.memory = pickle.load(open(load_memory, 'rb'))
+        self.min_experience = min_experience
 
         self.noise = utils.noise.OsuDiscreteNoise(mu=torch.tensor(0.5, device=device), sigma=torch.tensor(0.25, device=device), min_val=0.0, max_val=0.999)
 
@@ -155,12 +156,20 @@ class QTrainer:
         self.running_loss = 0.0
         self.running_counter = 0
 
+        self.gradient_clipping_norm = gradient_clipping_norm
+
         total_params = sum(p.numel() for p in self.q_network.parameters())
         print('Number of parameters: %d' % total_params)
 
     def select_action(self, state, controls_state):  # Greedy policy
         action = self.q_network(state, controls_state).detach()
         _, action = torch.max(action, 1)
+        return action
+
+    def random_action(self, x_discrete, y_discrete, click_dim=4):
+        x = self.noise()  # Normal distribution mean=0.5, clipped in [0, 1[ & uniform distrib
+        action = torch.tensor([int(x[0] * x_discrete) + x_discrete * int(
+            x[1] * y_discrete) + x_discrete * y_discrete * int(x[2] * click_dim)], device=device)
         return action
 
     def save_model(self, file_name, num=0):
@@ -172,15 +181,15 @@ class QTrainer:
         print('Loaded actor weights from: ' + weights_path)
         hard_copy(self.target_q_network, self.q_network)
 
-    def optimize(self): # TODO: GO through the funciton and check. CHECK TERMINAL STATES thing
-        if len(self.memory) < self.batch_size:
+    def optimize(self): # TODO: GO through the funciton and check. CHECK TERMINAL STATES thing. CLamp rewards and td error
+        if len(self.memory) < self.min_experience:
             return
         s1, a1, r1, s2, c_s1, c_s2 = self.memory.sample(self.batch_size)
         s = self.q_network(s1, c_s1)
         state_action_values = torch.stack([s[i, a1[i]] for i in range(self.batch_size)])  # Get estimated Q(s1,a1)
         next_state_values = self.target_q_network(s2, c_s2).detach().max(1)[0]
         expected_state_action_values = r1 + self.gamma * next_state_values
-        loss = torch.clamp(F.mse_loss(state_action_values, expected_state_action_values), 0, 1.5)
+        loss = torch.clamp(F.mse_loss(state_action_values, expected_state_action_values), 0, 1.0)
         self.running_loss += loss
         if self.running_counter % 200 == 0:
             self.plotter.step(self.running_loss/200)
@@ -189,9 +198,11 @@ class QTrainer:
         self.running_counter += 1
         self.optimizer.zero_grad()
         loss.backward()
-
+        '''
         for param in self.q_network.parameters():  # Todo: Benchmark to see whether this is faster than value_clip
             param.grad.data.clamp_(-0.1, 0.1)
+        '''
+        torch.nn.utils.clip_grad_norm_(self.q_network.parameters(), self.gradient_clipping_norm)
 
         self.optimizer.step()
 